6cb95285386194cdf701a1fbd48117c1accebe69
pavel.mikhailov <pamix1985@gmail.com>
2022-01-21 23:03:15 +0700

    split login and loginForm files, split users and usersList files.

---
+++ src/components/usersList.jsx
@@ -0,0 +1,154 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { paginate } from "../utils/paginate";
+import Pagination from "../components/pagination";
+import api from "../api";
+import GroupList from "../components/groupList";
+import SearchStatus from "../components/searchStatus";
+import UsersTable from "../components/usersTable";
+import _ from "lodash";
+import { useLocation } from "react-router-dom";
+import Search from "../components/search";
+
+const UsersList = () => {
+    const pageSize = 8;
+    const [currentPage, setCurrentPage] = useState(1);
+    const [professions, setProfessions] = useState();
+    const [selectedProf, setSelectedProf] = useState();
+    const [sortBy, setSortBy] = useState({ path: "name", order: "asc" });
+    const [caret, setCaret] = useState();
+    const [users, setUsers] = useState();
+    const [searchValue, setSearchValue] = useState("");
+    const location = useLocation();
+    const useQuery = () => new URLSearchParams(location.search);
+    const query = useQuery().get("search");
+
+    useEffect(() => {
+        setCaret({ selectedPath: sortBy.path, status: sortBy.order === "asc" });
+    }, [sortBy]);
+
+    useEffect(() => {
+        api.users.fetchAll().then((data) => setUsers(data));
+    }, []);
+
+    const handleDelete = (userId) => {
+        setUsers(users.filter((user) => user._id !== userId));
+    };
+
+    const handleToggleBookMark = (id) => {
+        setUsers(
+            users.map((user) => {
+                if (user._id === id) {
+                    return { ...user, bookmark: !user.bookmark };
+                }
+                return user;
+            })
+        );
+    };
+
+    const handleSearchChange = (value) => {
+        setSelectedProf();
+        setSearchValue(value);
+    };
+
+    useEffect(() => {
+        api.professions.fetchAll().then((data) => setProfessions(data));
+    }, []);
+
+    useEffect(() => {
+        setCurrentPage(1);
+    }, [selectedProf]);
+
+    const handleProfessionSelect = (item) => {
+        handleSearchChange("");
+        setSelectedProf(item);
+    };
+
+    const handlePageChange = (pageIndex) => {
+        setCurrentPage(pageIndex);
+    };
+
+    const handleSort = (item) => {
+        setSortBy(item);
+    };
+
+    const clearFilter = () => {
+        setSelectedProf();
+    };
+
+    if (users) {
+        let filteredUsers;
+
+        if (selectedProf) {
+            filteredUsers = users.filter(
+                (user) =>
+                    JSON.stringify(user.profession) ===
+                    JSON.stringify(selectedProf)
+            );
+        } else if (query) {
+            filteredUsers = users.filter(
+                (user) => user.name.toLowerCase().includes(query)
+            );
+        } else {
+            filteredUsers = users;
+        }
+
+        const count = filteredUsers.length;
+
+        const sortedUsers = _.orderBy(
+            filteredUsers,
+            [sortBy.path],
+            [sortBy.order]
+        );
+        const usersCrop = paginate(sortedUsers, currentPage, pageSize);
+
+        return (
+            <div className="d-flex">
+                {professions && (
+                    <div className="d-flex flex-column flex-shrink-0 p-3">
+                        <GroupList
+                            selectedItem={selectedProf}
+                            items={professions}
+                            onItemSelect={handleProfessionSelect}
+                        />
+                        <button
+                            className="btn btn-secondary mt-2"
+                            onClick={clearFilter}
+                        >
+                            {" "}
+                            Очистить
+                        </button>
+                    </div>
+                )}
+                <div className="d-flex flex-column">
+                    <SearchStatus length={count} />
+                    <Search value={searchValue} onChange={handleSearchChange} />
+                    {count > 0 && (
+                        <UsersTable
+                            users={usersCrop}
+                            onSort={handleSort}
+                            selectedSort={sortBy}
+                            caret={caret}
+                            onDelete={handleDelete}
+                            onToggleBookMark={handleToggleBookMark}
+                        />
+                    )}
+                    <div className="d-flex justify-content-center">
+                        <Pagination
+                            itemsCount={count}
+                            pageSize={pageSize}
+                            currentPage={currentPage}
+                            onPageChange={handlePageChange}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    return "loading...";
+};
+UsersList.propTypes = {
+    users: PropTypes.array
+};
+
+export default UsersList;
--- src/layouts/login.jsx
+++ src/layouts/login.jsx
@@ -1,86 +1,8 @@
-import React, { useEffect, useState } from "react";
-import TextField from "../components/textField";
-import { validator } from "../utils/validator";
+import React from "react";
+import LoginForm from "../components/loginForm";
 
 const Login = () => {
-    const [data, setData] = useState({ email: "", password: "" });
-    const [errors, setErrors] = useState({});
-    const isValid = Object.keys(errors).length !== 0;
-
-    const validatorConfig = {
-        email: {
-            isRequired: {
-                message: "Электронная почта обязательна для заполнения."
-            },
-            isEmail: {
-                message: "Электронная почта введена некорректно."
-            }
-        },
-        password: {
-            isRequired: {
-                message: "Пароль обязателен для заполнения."
-            },
-            isCapitalSymbol: {
-                message: "Пароль должен содержать минимум одну заглавную букву."
-            },
-            isDigit: {
-                message: "Пароль должен содержать минимум одну цифру."
-            },
-            min: {
-                message: "Минимальная длина пароля 8 символов.",
-                value: 8
-            }
-        }
-    };
-
-    const validate = () => {
-        const errors = validator(data, validatorConfig);
-        setErrors(errors);
-        return Object.keys(errors).length === 0;
-    };
-
-    useEffect(() => {
-        validate();
-    }, [data]);
-
-    const handleChange = ({ target }) => {
-        setData((prevState) => ({ ...prevState, [target.name]: target.value }));
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const isValid = validate();
-        if (!isValid) return;
-        console.log(data);
-    };
-
-    return (
-        <div className="container mt-5">
-            <div className="row">
-                <div className="col-md-6 offset-md-3 shadow p-4">
-                    <h3 className="mb-4">Login</h3>
-                    <form onSubmit={handleSubmit}>
-                        <TextField
-                            label="Электронная почта"
-                            value={data.email}
-                            onChange={handleChange}
-                            name="email"
-                            error={errors.email}
-                        />
-                        <TextField
-                            label="Пароль"
-                            type="password"
-                            value={data.password}
-                            onChange={handleChange}
-                            name="password"
-                            error={errors.password}
-                        />
-                        <button type="submit" disabled={isValid} className="btn btn-primary mx-auto w-100">Submit</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    );
+    return <LoginForm />;
 };
 
 export default Login;
