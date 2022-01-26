e456e77685da54f44c48d227082526ae770313ac
pavel.mikhailov <pamix1985@gmail.com>
2022-01-27 02:06:04 +0700

    create multiselect field component

---
+++ src/components/common/form/multiSelectField.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Select from "react-select";
+import PropTypes from "prop-types";
+
+const MultiSelectField = ({ label, options, onChange, placeholder, name }) => {
+    const optionsArray = (!Array.isArray(options) && typeof options === "object")
+        ? Object.keys(options).map((optionName) => {
+            return { value: options[optionName]._id, label: options[optionName].name };
+        })
+        : options;
+
+    const handleChange = (value) => {
+        onChange({ name, value });
+    };
+
+    return (
+        <div className="mb-4">
+            <label>{ label }</label>
+            <Select
+                options={optionsArray}
+                onChange={handleChange}
+                isMulti
+                className="basic-multi-select"
+                classNamePrefix="select"
+                placeholder={placeholder}
+                closeMenuOnSelect={false}
+            />
+        </div>
+    );
+};
+
+MultiSelectField.propTypes = {
+    label: PropTypes.string,
+    options: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired,
+    onChange: PropTypes.func.isRequired,
+    placeholder: PropTypes.string,
+    name: PropTypes.string
+};
+
+export default MultiSelectField;
