afa6aff2a058d5aa82a95abb3ef5a73e435257cb
Pavel Mikhajlov <pamix1985@gmail.com>
2022-01-27 22:01:53 +0700

    added stayOn checkbox

--- src/components/ui/loginForm.jsx
+++ src/components/ui/loginForm.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import TextField from "../common/form/textField";
+import CheckBoxField from "../common/form/checkBoxField";
 import { validator } from "../../utils/validator";
 
 const LoginForm = () => {
-    const [data, setData] = useState({ email: "", password: "" });
+    const [data, setData] = useState({ email: "", password: "", stayOn: false });
     const [errors, setErrors] = useState({});
     const isValid = Object.keys(errors).length !== 0;
 
@@ -43,8 +44,10 @@ const LoginForm = () => {
         validate();
     }, [data]);
 
-    const handleChange = ({ target }) => {
-        setData((prevState) => ({ ...prevState, [target.name]: target.value }));
+    const handleChange = (target) => {
+        if (target) {
+            setData((prevState) => ({ ...prevState, [target.name]: target.value }));
+        }
     };
 
     const handleSubmit = (e) => {
@@ -71,6 +74,9 @@ const LoginForm = () => {
                 name="password"
                 error={errors.password}
             />
+            <CheckBoxField value={ data.stayOn } onChange={ handleChange } name="stayOn">
+                Оставаться в системе
+            </CheckBoxField>
             <button type="submit" disabled={isValid} className="btn btn-primary mx-auto w-100">Войти</button>
         </form>
     );
